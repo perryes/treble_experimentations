03b76d27c6157486de75288c8a4f2236966156fe
Pierre-Hugues Husson <phh@phh.me>
2018-08-27 20:52:45 +0200

    Detect if rom has exfat. delete it from our own manifest in that case

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -256,6 +256,10 @@ function init_patches() {
         # patches by hand
         rm -f .repo/local_manifests/replace.xml
 
+	if grep -qF exfat .repo/manifest.xml;then
+		sed -i -E '/external\/exfat/d' .repo/local_manifests/manifest.xml
+	fi
+
         # should I do this? will it interfere with building non-gapps images?
         # rm -f .repo/local_manifests/opengapps.xml
     fi
